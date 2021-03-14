80478b1189d9be62e9f8c3924e8f5643cbb8a2e7
Kendall Furrow <69933604+kfurrow7@users.noreply.github.com>
2021-03-13 20:42:44 -0800

    hw

---
+++ Assignment 9/sketch.js
@@ -0,0 +1,55 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background(154,149,143);
+    textSize(22)
+    text("Hello!", 10,80);
+
+    // head
+    fill(250, 229, 211);
+    circle(250,100,175);
+
+    // eyes
+    strokeWeight(7);
+    circle(275,60,30);
+    circle(220,60,30);
+
+
+    // nose
+    point(245,90);
+
+    // mouth
+    ellipse(245, 135, 30, 45)
+
+    // hair
+    fill(52,152,219);
+    line(130,175,175,50);
+    line(325,50,360,175);
+    // body
+    fill(255);
+
+
+  //dress
+    fill(78,120,143);
+    strokeWeight(15);
+    triangle(220,320,250,220,280,320);
+// right arm
+    fill(10, 24, 120);
+    rect(300,195,50,10);
+    // left arm
+    rect(150,195,50,10);
+    // left leg
+    rect(200,335,10,50);
+    // right leg
+    rect(290,335,10,50);
+
+    fill(227,2,122);
+    textSize(22);
+    text("Kendall Furrow",270,500 );
+
+
+}
